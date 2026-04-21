1d873d2c8c4fc11df16700e5c3234e3be18e47b2
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-04-21 08:26:11 +0600

    added show button and display show details

--- script/index.js
+++ script/index.js
@@ -1,32 +1,53 @@
+function removeActiveClass() {
+    const activeBtn = document.getElementsByClassName("active");
+    for (let active of activeBtn) {
+        active.classList.remove("active")
+    }
+}
 
-
-function loadCategories () {
+function loadCategories() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then(res =>res.json())
-    .then(data => displayCategories(data.categories)
-    )
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories)
+        )
 }
 
-function displayCategories(categories){ 
-const categoriContainer = document.getElementById("categories-container");
+function displayCategories(categories) {
+    const categoriContainer = document.getElementById("categories-container");
 
-for(const cat of categories){
-const div = document.createElement("div")
-div.innerHTML = `
- <button class="btn-sm btn hover:bg-[#FF1F3D] hover:text-white">${cat. category}</button>
+    for (const cat of categories) {
+        const div = document.createElement("div")
+        div.innerHTML = `
+ <button id='btn-${cat.category_id}' onclick="loadCategoriesVideo(${cat.category_id})" class="btn-sm btn hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
 `
-categoriContainer.append(div)
-}
+        categoriContainer.append(div)
+    }
 }
 
-function loadVideo (){
+function loadVideo() {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(response => response.json())
-    .then(data => displayVideo(data.videos))
+        .then(response => response.json())
+        .then(data => {
+            removeActiveClass()
+            document.getElementById('all-btn').classList.add("active")
+            displayVideo(data.videos)
+        })
 }
-const displayVideo = (videos) =>{
+const displayVideo = (videos) => {
     const videoContainer = document.getElementById("video-container");
-    videos.forEach((video =>{
+    videoContainer.innerText = " ";
+
+    if (videos.length == 0) {
+        videoContainer.innerHTML = `
+        <div class="col-span-full flex flex-col justify-center items-center text-center py-20">
+        <img class="w-[120px]" src="assest/Icon.png" alt="">
+        <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
+</div>
+        `
+        return;
+    }
+
+    videos.forEach((video => {
         const videoBox = document.createElement("div")
         videoBox.innerHTML = `
         <div class="card bg-base-100">
@@ -52,12 +73,51 @@ const displayVideo = (videos) =>{
     <p class="text-sm text-gray-400 ">${video.others.views} view</p>
 </div>
   </div>
+  <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">show details</button>
 </div>
         `
         videoContainer.append(videoBox)
-    })) 
+    }))
+}
+const loadCategoriesVideo = (id) => {
+    const url = ` https://openapi.programming-hero.com/api/phero-tube/category/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            removeActiveClass()
+            const clickedBtn = document.getElementById(`btn-${id}`)
+            clickedBtn.classList.add("active")
+
+            displayVideo(data.category)
+        })
 }
 
+const loadVideoDetails = (videoID) =>{
+const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoID}`
+fetch(url)
+.then(res => res.json())
+.then(data =>displayVideoDetails(data.video)
+)
+}
+
+const displayVideoDetails = (video) =>{
+    console.log(video);
+     document.getElementById("videoDetails").showModal();
+     const detailsContainer = document.getElementById("details-container")
+     detailsContainer.innerHTML=`
+     <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img class="w-full "
+      src="${video.thumbnail}"
+      alt="thumbnail" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+
+  </div>
+</div>
+     `
+}
 
 loadCategories()
-loadVideo()
